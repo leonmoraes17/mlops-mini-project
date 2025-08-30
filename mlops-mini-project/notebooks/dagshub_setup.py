967ec1fe24e8a9439ffb0c73ebd94dd906f37a05
leonmoraes17 <leonm4787@gmail.com>
2025-08-30 19:25:50 -0400

    add experiment 1 Logistic Reg Baseline Model

---
+++ mlops-mini-project/notebooks/dagshub_setup.py
@@ -0,0 +1,12 @@
+import mlflow
+import dagshub
+
+mlflow.set_tracking_uri("https://dagshub.com/leonmoraes17/mlops-mini-project.mlflow")
+dagshub.init(repo_owner='leonmoraes17', repo_name='mlops-mini-project', mlflow=True)
+
+
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+
+  
